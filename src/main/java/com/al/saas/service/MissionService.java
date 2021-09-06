7e481e9e24c1d6241a44185ae6b7285a1943790e
alex <17674745183@163.com>
2021-09-06 13:50:16 +0800

    添加注释

--- src/main/java/com/al/saas/service/MissionService.java
+++ src/main/java/com/al/saas/service/MissionService.java
@@ -1,5 +1,6 @@
 package com.al.saas.service;
 
+import com.al.saas.consist.MissionExecution;
 import com.al.saas.domain.Mission;
 
 import java.util.List;
@@ -16,26 +17,26 @@ public interface MissionService {
      * 查看所有任务
      * @return
      */
-    List<Mission> getMissionList();
+    MissionExecution getMissionList();
 
     /**
      * 新建任务
      * @param mission 任务实体类
      * @return 0：新建成功 1：新建失败
      */
-    int addMission(Mission mission);
+    MissionExecution addMission(Mission mission);
 
     /**
      * 编辑任务
      * @param mission 任务实体类
      * @return 0：编辑成功 1：编辑失败
      */
-    int modifyMission(Mission mission);
+    MissionExecution modifyMission(Mission mission);
 
     /**
      * 终止任务(将可用状态置为0)
      * @param mission 任务实体类
      * @return 0：终止成功 1：终止失败
      */
-    int abortMission(Mission mission);
+    MissionExecution abortMission(Mission mission);
 }
