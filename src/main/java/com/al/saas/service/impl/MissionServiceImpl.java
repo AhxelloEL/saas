131dfc4e6aa1981724aa71b0c40c20302c74e78e
alex <17674745183@163.com>
2021-09-06 13:49:09 +0800

    任务调度接口实现

--- src/main/java/com/al/saas/service/impl/MissionServiceImpl.java
+++ src/main/java/com/al/saas/service/impl/MissionServiceImpl.java
@@ -1,9 +1,14 @@
 package com.al.saas.service.impl;
 
+import com.al.saas.consist.GradeExecution;
+import com.al.saas.consist.MissionExecution;
+import com.al.saas.constant.GradeStatusEnum;
+import com.al.saas.constant.MissionStatusEnum;
 import com.al.saas.domain.Mission;
 import com.al.saas.repository.MissionDao;
 import com.al.saas.service.MissionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
@@ -15,44 +20,76 @@ import java.util.List;
  * @Description 任务调度接口实现
  * @date2021/9/4 13:25
  */
+@Service
 public class MissionServiceImpl implements MissionService {
     @Autowired
     private MissionDao missionDao;
 
+
     @Override
-    public List<Mission> getMissionList() {
-        return missionDao.queryMissionList();
+    public MissionExecution getMissionList() {
+        List<Mission> missionList = missionDao.queryMissionList();
+        MissionExecution me = new MissionExecution();
+        if (missionList != null){
+            me.setMissionList(missionList);
+        }
+        return me;
     }
 
     @Override
-    public int addMission(Mission mission) {
+    public MissionExecution addMission(Mission mission) {
         if (mission != null && mission.getMissionId() != null){
-            //初始化发布时间
-            mission.setCreateTime(new Date());
-            missionDao.addMission(mission);
+           try {
+               mission.setCreateTime(new Date());
+               int effectNum = missionDao.addMission(mission);
+               if (effectNum <= 0){
+                   return new MissionExecution(MissionStatusEnum.INNER_ERROR);
+               }
+           }catch (Exception e){
+                throw new RuntimeException("新建任务失败："+e.getMessage());
+           }
         }else {
-            return 0;
+            return new MissionExecution(MissionStatusEnum.EMPTY);
         }
-        return 1;
+        return new MissionExecution(MissionStatusEnum.SUCCESS,mission);
     }
 
     @Override
-    public int modifyMission(Mission mission) {
+    public MissionExecution modifyMission(Mission mission) {
         if (mission != null && mission.getMissionId() != null){
-            missionDao.redactMission(mission);
-        }else {
-            return 0;
+            return new MissionExecution(MissionStatusEnum.EMPTY);
         }
-        return 1;
+        try {
+            mission.setLastEditTime(new Date());
+            int effectNum = missionDao.redactMission(mission);
+            if (effectNum <= 0){
+                return new MissionExecution(MissionStatusEnum.INNER_ERROR);
+            }else {
+                //TODO
+                mission = missionDao.selectById(mission.getMissionId());
+                return new MissionExecution(MissionStatusEnum.SUCCESS,mission);
+            }
+        }catch (Exception e){
+            throw new RuntimeException("更新任务信息失败："+e.getMessage());
+        }
     }
 
     @Override
-    public int abortMission(Mission mission) {
+    public MissionExecution abortMission(Mission mission) {
         if (mission != null && mission.getMissionId() != null){
-            missionDao.abortMission(mission);
-        }else {
-            return 0;
+            return new MissionExecution(MissionStatusEnum.EMPTY);
         }
-        return 1;
+        try {
+            int effectNum = missionDao.abortMission(mission);
+            if (effectNum <= 0){
+                return new MissionExecution(MissionStatusEnum.INNER_ERROR);
+            }else {
+                //TODO
+                mission = missionDao.selectById(mission.getMissionId());
+                return new MissionExecution(MissionStatusEnum.SUCCESS,mission);
+            }
+        }catch (Exception e){
+            throw new RuntimeException("终止任务信息失败："+e.getMessage());
+        }
     }
 }
